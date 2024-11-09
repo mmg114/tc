6fe98ea6259f404d13ee92105b9d27ae176a1aaa
Mauricio Muñoz Guzman <mmg.usb@gmail.com>
2024-11-08 20:52:03 -0500

    models

---
+++ src/main/java/com/parquesoftti/tc/model/CreditCard.java
@@ -0,0 +1,31 @@
+package com.parquesoftti.tc.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "CreditCard")
+public class CreditCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 16)
+    private String cardNumber;
+
+    @Column(nullable = false)
+    private String expirationDate;
+
+    @Column(nullable = false, length = 4)
+    private String cvv;
+
+}
