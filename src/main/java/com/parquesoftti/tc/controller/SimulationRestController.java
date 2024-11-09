7529c0f76c1e9560245fba6d27ec05f281962548
Mauricio Muñoz Guzman <mmg.usb@gmail.com>
2024-11-08 19:54:21 -0500

    init

---
+++ src/main/java/com/parquesoftti/tc/controller/SimulationRestController.java
@@ -0,0 +1,33 @@
+package com.parquesoftti.tc.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SimulationRestController {
+
+        @PostMapping()
+        public ResponseEntity<String> payment(){
+            return ResponseEntity.ok("Hola mundo");
+        }
+
+        @PutMapping()
+        public ResponseEntity<String> reversePayment(){
+            return null;
+        }
+
+
+        @DeleteMapping()
+        public ResponseEntity<String> deleteCard(){
+            return null;
+        }
+
+
+        @GetMapping()
+        public ResponseEntity<String> getCard(){
+            return null;
+        }
+
+
+}
